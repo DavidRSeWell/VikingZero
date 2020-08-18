DS - MCTS seems to be working on TicTacToe vs random player. Had problems with correct backprop. Will need to clean up

src/vikingzero/agents/connect4_agent.py
@@ -31,10 +31,12 @@ class RandomConnect4Agent:
 
 class Connect4MinMax(MINIMAX):
 
-    def __init__(self,env: Connect4,player = 1,type="minimax"):
+    def __init__(self,env: Connect4,player = 1,type="minimax",depth=2):
         super().__init__()
 
+        print(f"Loading MINMAX agent with type = {type}")
         self._env = env
+        self._d = depth
         self._player = player
         self._type = type
 
@@ -42,7 +44,7 @@ class Connect4MinMax(MINIMAX):
 
         s = Connect4Node(self._env,board,self._player)
 
-        a = self.run(s,type=self._type)
+        a = self.run(s,type=self._type,d=self._d)
 
         return a
 
@@ -106,6 +108,8 @@ class Connect4MCTS(MCTS):
 
     def __init__(self,env: Connect4, n_sim: int = 50,c: int = 1,player = 1):
         super().__init__(c)
+
+        print(f"Loaded MCTS with nsim = {n_sim} c = {c} player = {player}")
         self._env = env
         self._num_sim = n_sim
         self._player = player
@@ -126,16 +130,33 @@ class Connect4MCTS(MCTS):
     def get_max_action(self,s) -> int:
 
         if s not in self.children:
-            raise Exception(f'Node {s} does not exist in the tree')
+            raise Exception(f"Node {s} does not exist in the tree")
 
         if self._env.check_winner(s.board):
-            raise Exception('Attempting to compute action on a leaf node')
+            raise Exception("Attempting to compute action on a leaf node")
 
         children = self.children
 
         values = [self._Q[c] / self._N[c] for c in children[s]]
 
-        return np.argmax(np.array(values))
+        max_child = children[s][np.argmax(np.array(values))]
+
+        action = self.get_parent_ation(s,max_child)
+
+        return action
+
+    def get_parent_ation(self,parent,child):
+
+        a = parent.board
+
+        b = child.board
+
+        diff = a - b
+        diffs = np.where(diff != 0)
+
+        action = diffs[1][0]
+
+        return action
 
     def select_child(self,node):
 
@@ -167,8 +188,8 @@ class UCIOracle:
     @classmethod
     def load_from_disk(cls,env,nn_path,hidden_layer_size):
 
-        inputDim = 42  # takes variable 'x'
-        outputDim = 1  # takes variable 'y'
+        inputDim = 42  # takes variable "x"
+        outputDim = 1  # takes variable "y"
 
         model = UCINet(inputDim, hidden_layer_size, outputDim)
 
@@ -213,12 +234,12 @@ class UCIOracle:
             self._pi[self.hash_numpy(board)] = max_a
 
     def convert_to_numpy(self,x):
-        '''
+        """
         Function to take the UCI format for connect 4
         and convert it to numpy.
         :param x:
         :return:
-        '''
+        """
 
         # TODO this assumes a fixed board dim
         new_x = np.zeros((6, 7))
@@ -268,26 +289,26 @@ class UCIOracle:
         return hash(x.data.tobytes())
 
     def process_data(self,data_path):
-        '''
+        """
         Read in .data file and return structured
         data in numpy array
         :param data_path:
         :return:
-        '''
-        data_file = open(data_path, 'r')
+        """
+        data_file = open(data_path, "r")
 
         data = data_file.readlines()
 
         X_data, Y_data = [], []
         for i, d in enumerate(data):
 
-            a = d.replace('b', '0').replace('x', '1').replace('o', '2').replace('\n', '').split(',')
+            a = d.replace("b", "0").replace("x", "1").replace("o", "2").replace("\n", "").split(",")
             X = a[:-1]
             X = self.convert_to_numpy(X)
             Y = -1.0
-            if 'win' in a[-1]:
+            if "win" in a[-1]:
                 Y = 1.0
-            elif 'draw' in a[-1]:
+            elif "draw" in a[-1]:
                 Y = 0.0
             X_data.append(X)
             Y_data.append(Y)

src/vikingzero/agents/tictactoe_agents.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 from ..environments.tictactoe_env import TicTacToe
-from ..search import Node,MINIMAX
+from ..search import MCTS,MINIMAX,Node
+
 
 class HumanAgent:
 
     def __init__(self,env):
         self._env = env
 
-    def act(self,s):
+    def act(self,s,r):
 
         while True:
             try:
@@ -23,6 +24,7 @@ class HumanAgent:
 
                 print(f"action is invalid valid action are")
 
+
 class RandomTicTacToeAgent:
     """
     Agent that acts randomly while playing Connect4
@@ -31,7 +33,7 @@ class RandomTicTacToeAgent:
 
         self._env = env
 
-    def act(self,s):
+    def act(self,s,r):
         """
         Take an action given a state
         :param s:
@@ -40,6 +42,7 @@ class RandomTicTacToeAgent:
         valid_actions = np.where(self._env.board == 0.0)[0]
 
         return np.random.choice(valid_actions,1)[0]
+
 
 class TicTacToeMinMax(MINIMAX):
 
@@ -58,6 +61,7 @@ class TicTacToeMinMax(MINIMAX):
 
         return a
 
+
 class TicTacToeNode(Node):
 
     def __init__(self,env: TicTacToe,board: np.array, player: int, winner = 0):
@@ -71,6 +75,9 @@ class TicTacToeNode(Node):
         children = self.get_children()
         random_child = np.random.choice(children, 1)[0]
         assert type(random_child) == TicTacToeNode
+        if random_child.player == self.player:
+            print("wtf")
+            raise Exception("")
         return random_child
 
     def get_children(self):
@@ -80,8 +87,11 @@ class TicTacToeNode(Node):
         children = []
         player = 2 if self.player == 1 else 1
         for a in valid_actions:
+
             next_board = self.env.next_state(curr_board,a, self.player)
             r, winner = self.env.check_winner(next_board)
+            if winner == player:
+                print('wtf')
             next_node = TicTacToeNode(env=self.env, board=next_board, player=player, winner=winner)
             children.append(next_node)
         return children
@@ -92,13 +102,15 @@ class TicTacToeNode(Node):
     def next_state(self,a):
         return self.env.next_state(self.board,a,self.player)
 
-    def reward(self,player):
-        reward = 1
+    def reward(self):
         if self.winner == -1:
-            return 0
-        elif int(self.winner) != player:
-            reward = -1
-        return reward
+            #TODO
+            return (-1,0.5)
+        elif int(self.winner) != self.player:
+            reward = 1
+            return (self.winner,reward)
+        else:
+            print("wtf")
 
     @property
     def actions(self):
@@ -109,4 +121,106 @@ class TicTacToeNode(Node):
 
     def __hash__(self):
         return hash((self.board.data.tobytes(),self.player,self.winner))
+
+
+class TicTacToeMCTS(MCTS):
+
+    def __init__(self,env: TicTacToe, n_sim: int = 50,c: int = 1,player = 1):
+        super().__init__(c)
+
+        print(f"Loaded MCTS with nsim = {n_sim} c = {c} player = {player}")
+        self._env = env
+        self._num_sim = n_sim
+        self._player = player
+
+    def act(self,board,render):
+
+        s = TicTacToeNode(self._env,board,self._player,0)
+        # First rum simulations to collect
+        # Tree statistics
+        for _ in range(self._num_sim):
+
+            self.run(s)
+
+        a = self.get_max_action(s,render)
+
+        return a
+
+    def create_value_board(self,parent,children):
+
+        board = parent.board.copy()
+        board = np.ones(board.shape)*-np.inf
+        board2 = np.ones(board.shape)*-np.inf
+        for c in children[parent]:
+            v = self.value(c)
+            a = self.get_parent_ation(parent,c)
+            board[a] = self._Q[c]
+            board2[a] = self._N[c]
+
+        return board,board2
+
+    def get_max_action(self,s,render) -> int:
+
+        if s not in self.children:
+            raise Exception(f"Node {s} does not exist in the tree")
+
+        if self._env.is_win(s.board):
+            raise Exception("Attempting to compute action on a leaf node")
+
+        children = self.children
+
+        values = [self.value(c) for c in children[s]]
+
+        if render:
+
+            print('------- CURRENT BOARD -------')
+            print(s.board.reshape((3,3)))
+
+            print('-------- VALUES of BOARD --------')
+            v_board,n_board = self.create_value_board(s,children)
+            print(v_board.reshape((3,3)))
+
+            print('-------- COUNTS ----------------')
+            print(n_board.reshape((3,3)))
+
+        max_child = children[s][np.argmax(np.array(values))]
+
+        action = self.get_parent_ation(s,max_child)
+
+        return action
+
+    def get_parent_ation(self,parent,child):
+
+        a = parent.board
+
+        b = child.board
+
+        diff = a - b
+        diffs = np.where(diff != 0)
+
+        action = diffs[0][0]
+
+        return action
+
+    def select_child(self,node):
+
+        # All children of node should already be expanded:
+        assert all(n in self.children for n in self.children[node])
+
+        log_N_vertex = np.log(self._N[node])
+
+        def uct(n):
+            "Upper confidence bound for trees"
+            return self._Q[n] / self._N[n] + self.c * np.sqrt(
+                log_N_vertex / self._N[n]
+            )
+
+        return max(self.children[node], key=uct)
+
+    def value(self,s):
+
+        if self._N[s] == 0:
+            return 0 #TODO return 0 or -inf?
+
+        return self._Q[s] / self._N[s]
 

src/vikingzero/environments/tictactoe_env.py
@@ -18,10 +18,16 @@ class TicTacToe:
         self.board = np.zeros(9,)
 
         def run_test():
-            self.board[0] = 1
-            self.board[1] = 1
-            self.board[2] = 2
-            self.board[4] = 2
+            board = [[2., 2., 1.],
+                    [0., 2., 1.],
+                    [0.,1.,0.]]
+
+            self.board = np.array(board).flatten()
+
+            #self.board[0] = 1
+            #self.board[1] = 1
+            #self.board[2] = 2
+            #self.board[4] = 2
             #self.board[7] = 1
             #self.board[8] = 2
 
@@ -64,10 +70,15 @@ class TicTacToe:
         self.board = np.zeros(9,)
 
         def run_test():
-            self.board[0] = 1
-            self.board[1] = 1
-            self.board[2] = 2
-            self.board[4] = 2
+            board = [[2., 2., 1.],
+                          [0., 2., 1.],
+                          [0., 1., 0.]]
+
+            self.board = np.array(board).flatten()
+            #self.board[0] = 1
+            #self.board[1] = 1
+            #self.board[2] = 2
+            #self.board[4] = 2
             # self.board[7] = 1
             # self.board[8] = 2
 

src/vikingzero/search.py
@@ -33,10 +33,12 @@ class MCTS:
         to the current leaf node
         :return:
         """
+        winner,r = reward
         for node in reversed(path):
             self._N[node] += 1
-            self._Q[node] += reward
-            reward = 1 - reward  # 1 for me is 0 for my enemy, and vice versa
+            if node.player != winner:
+                self._Q[node] += r
+            #reward = 1 - reward  # 1 for me is 0 for my enemy, and vice versa
 
     def expand(self,node):
         """
@@ -45,7 +47,7 @@ class MCTS:
         :param node:
         :return:
         """
-        if node in self.children:
+        if (node in self.children) or node.is_terminal():
             return  # already expanded
         self.children[node] = node.get_children()
 
@@ -55,10 +57,13 @@ class MCTS:
         based on some simulate mechanism. Could be rollout or could be a NN
         :return:
         """
-        player = node.player
+        #player = node.player
         while True:
             if node.is_terminal():
-                reward = node.reward(player)
+                reward = node.reward()
+                #return reward
+                if not reward:
+                    print('huh')
                 return reward
             node = node.find_random_child()
 
@@ -209,7 +214,7 @@ class MINIMAX:
 
         return best_action
 
-    def alpha_beta_depth(self,node,cutoff_test=None):
+    def alpha_beta_depth(self,node,cutoff_test=None,n=5):
 
         player = node.player
 
@@ -217,7 +222,7 @@ class MINIMAX:
 
             if cutoff_test(child_node,depth):
 
-                return self.simulate(child_node,player)
+                return self.simulate(child_node,player,n=n)
 
             v = -np.inf
             for child in child_node.get_children():
@@ -234,7 +239,7 @@ class MINIMAX:
         def min_value(child_node, alpha, beta, depth):
 
             if cutoff_test(child_node,depth):
-                return self.simulate(child_node,player)
+                return self.simulate(child_node,player,n=n)
 
             v = np.inf
             for child in child_node.get_children():
@@ -261,7 +266,7 @@ class MINIMAX:
         print(vs)
         return best_action
 
-    def run(self,node,type="minimax",d=2):
+    def run(self,node,type="minimax",d=2,n=5):
 
         if node in self.children:
             return self.policy[node]
@@ -282,7 +287,7 @@ class MINIMAX:
 
                 cutoff_test = lambda node, depth: depth > d or node.is_terminal()
 
-                max_child = self.alpha_beta_depth(node,cutoff_test=cutoff_test)
+                max_child = self.alpha_beta_depth(node,cutoff_test=cutoff_test,n=n)
 
             else:
                 raise Exception(f"Inccorect minimax alogrithm type {type}")

tests/agents/test_connect4_agent.py
@@ -1,4 +1,6 @@
-from vikingzero.agents.connect4_agent import RandomConnect4Agent,Connect4MinMax
+import numpy as np
+
+from vikingzero.agents.connect4_agent import RandomConnect4Agent,Connect4MinMax,Connect4MCTS
 from vikingzero.designers.connect4_designer import Connect4Designer
 from vikingzero.environments.connect4_env import Connect4
 
@@ -42,12 +44,15 @@ def test_designer():
     }
 
     agent2_config = {
-        "agent": RandomConnect4Agent,
-        #"player": 2,
+        "agent": Connect4MCTS,
+        "player": 2,
+        "c":np.sqrt(2),
+        "n_sim":200
+
         #"type": "alphabeta_depth"
     }
 
-    designer = Connect4Designer(iters=10,env=env
+    designer = Connect4Designer(iters=100,env=env
                                 ,agent1_config=agent1_config
                                 ,agent2_config=agent2_config
                                 )